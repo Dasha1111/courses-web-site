fbc6dfb8a26e32f0a3787f4648df30917df0a1dd
Dasha Malyshenko <maldasha2@gmail.com>
2020-06-28 21:01:54 +0300

    What we do

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function() {
+    /* Collapse */
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+
+        $this.toggleClass("active");
+    });
+
+
+    /* Slider */
+    $("[data-slider]").slick({
+        infinite: true,
+        fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
